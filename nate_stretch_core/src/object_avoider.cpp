19e86a1371e46d6233d9bfa1fc4680c1bc5f4d99
NathanGere <Nathan_Gere@student.uml.edu>
2022-07-07 10:53:19 -0400

    parameterized object_avoider and added navigation files

--- nate_stretch_core/src/object_avoider.cpp
+++ nate_stretch_core/src/object_avoider.cpp
@@ -3,6 +3,10 @@
 #include<geometry_msgs/Twist.h>
 
 ros::Publisher pub;
+std::string cmd_vel_output;
+std::string laser_scan;
+float forward_speed;
+float backward_speed;
 
 void callback(const sensor_msgs::LaserScan::ConstPtr &scan)
 {
@@ -28,12 +32,12 @@ void callback(const sensor_msgs::LaserScan::ConstPtr &scan)
     switch(tracker)
     {
         case 0:
-            motorizer.linear.x = 0.5;
+            motorizer.linear.x = forward_speed;
         case 1:
-            motorizer.linear.x = -0.5;
+            motorizer.linear.x = backward_speed;
             break;
         case 2:
-            motorizer.linear.x = 0.5;
+            motorizer.linear.x = forward_speed;
             break;
         case 3:
             motorizer.linear.x = 0.0;
@@ -46,10 +50,23 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "object_avoider_node");
     ros::NodeHandle n;
-    pub = n.advertise<geometry_msgs::Twist>("/stretch_diff_drive_controller/cmd_vel", 1000);
-    ros::Subscriber sub = n.subscribe("/scan", 1000, callback);
 
-    ros::spin();
+    n.param<std::string>("cmd_vel_output", cmd_vel_output, "/stretch_diff_drive_controller/cmd_vel");
+    n.param<std::string>("laser_scan", laser_scan, "/scan");
+
+    pub = n.advertise<geometry_msgs::Twist>(cmd_vel_output, 1000);
+    ros::Subscriber sub = n.subscribe(laser_scan, 1000, callback);
+
+    ros::Rate loop_rate(10);
+    while(ros::ok())
+    {   
+        n.param<float>("forward_speed", forward_speed, 0.5);
+        n.param<float>("backward_speed", backward_speed, -0.5);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    
+    return 0;
 }
 
  /*
---
+++ prep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/csrobot/stretch_ws
+catkin_make
+source devel/setup.bash
